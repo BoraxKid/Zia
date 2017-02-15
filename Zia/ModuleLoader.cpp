6875670f8291a16715fa684ea82707d4902a6fa4
K6 <chloe.cano@epitech.eu>
2017-02-15 16:06:19 +0100

    mod load

--- Zia/ModuleLoader.cpp
+++ Zia/ModuleLoader.cpp
@@ -1,11 +1,15 @@
-#include <string>
 #include "ModuleLoader.h"
-
 
 namespace Zia
 {
 	ModuleLoader::ModuleLoader(const std::string & basedir)
+		: _basedir(basedir + '/')
 	{
+		DIR		*dir;
+		if ((dir = opendir(this->_basedir.c_str())))
+			closedir(dir);
+		else
+			throw (Zia::ModuleLoaderException("Cannot open modules basedir."));
 	}
 
 	ModuleLoader::~ModuleLoader()
@@ -14,11 +18,84 @@ namespace Zia
 
 	bool ModuleLoader::loadModule(const std::string & name)
 	{
-		return false;
+		ModuleDL		*mod = NULL;
+		t_modInstance	modInst = NULL;
+	
+		if (this->_modules.count(name))
+		{
+			this->_logger.warning("Module \'" + name + "\' is already loaded.");
+			return (false);
+		}
+		std::string pname = std::string(this->_basedir + name + LIB_EXT);
+		try {
+			mod = new ModuleDL(pname);
+			modInst = (t_modInstance)mod->getSym("instantiate");
+		} catch (ModuleDLException const &me) {
+			delete (mod);
+			throw (ModuleDLException(std::string(me.what()) + " for module \'" + name + "\'"));
+		}
+		this->_modules[name] = std::make_pair(mod, modInst());
+		return (true);
 	}
 
 	bool ModuleLoader::unloadModule(const std::string & name)
 	{
-		return false;
+		if (this->_modules.count(name) < 1)
+		{
+			this->_logger.warning("Module \'" + name + "\' is not loaded but an attempt to unload it was made.");
+			return (false);
+		}
+		delete (std::get<1>(this->_modules[name]));
+		delete (std::get<0>(this->_modules[name]));
+		this->_modules.erase(name);
+		return (true);
 	}
+
+#ifdef linux
+
+	ModuleDL::ModuleDL(const std::string &pname)
+	{
+		if (!(this->_hdl = dlopen(pname.c_str(), RTLD_LAZY)))
+			throw (ModuleDLException("Failed to load library \'" + pname + "\'"));
+	}
+
+	void		*ModuleDL::getSym(const std::string &sym)
+	{
+		void	*mod;
+
+		if (!(mod = dlsym(this->_hdl, sym.c_str())))
+			throw (ModuleDLException("symbol \'" + sym + "\' not found"));
+		return (mod);
+	}
+
+	ModuleDL::~ModuleDL()
+	{
+		dlclose(this->_hdl);
+	}
+
+#else
+	
+	ModuleDL::ModuleDL(const std::string &pname)
+	{
+		this->_hdl = LoadLibrary(pname.c_str());
+		if (!this->hdl)
+			throw (ModuleDLException("Failed to load library \'" + pname + "\'"));
+	}
+
+	void		*ModuleDL::getSym(const std::string &sym)
+	{
+		void	*mod;
+		
+		if (!(mod = GetProcAddress(sym, this->_hdl)))
+			throw (ModuleDLException("symbol \'" + sym + "\' not found"));
+		return (mod);
+	}
+
+	ModuleDL::~ModuleDL()
+	{
+		FreeLibrary(this->_hdl);
+	}
+
+#endif
+
 }
--- Zia/ModuleLoader.h
+++ Zia/ModuleLoader.h
@@ -1,18 +1,82 @@
 #pragma once
 
+#include <dirent.h>
+
 #include <vector>
+#include <string>
+#include <map>
+#include <fstream>
 
+#ifdef linux
 
-namespace Zia
+# define LIB_EXT	".so"
+# include <dlfcn.h>
+# define DL_HDL		void *
+
+#else
+
+# define LIB_EXT	".dll"
+# include <windows.h>
+# define DL_HDL		HMODULE
+
+#endif
+
+#include <Module/AModule.hpp>
+#include <Logger/Logger.hh>
+
+typedef apouche::AModule	*(*t_modInstance)();
+
+namespace		Zia
 {
-	class ModuleLoader
+
+	class ModuleDLException : public std::exception
+	{
+		public:
+			ModuleDLException(std::string what) throw() : _what(what) {  }
+			~ModuleDLException() {  }
+
+        virtual const char  *what() const throw() { return _what.c_str(); }
+
+		private:
+			std::string _what;
+	};
+
+	class					ModuleDL
+	{
+		private:
+			DL_HDL			_hdl;
+
+		public:
+			ModuleDL(const std::string &pname);
+			~ModuleDL();
+			void		*getSym(const std::string &sym);
+	};
+
+	class ModuleLoaderException : public std::exception
+	{
+		public:
+			ModuleLoaderException(std::string what) throw() : _what(what) {  }
+			~ModuleLoaderException() {  }
+
+        virtual const char  *what() const throw() { return _what.c_str(); }
+
+		private:
+			std::string _what;
+	};
+
+	class		ModuleLoader
 	{
 	public:
 		ModuleLoader(const std::string &basedir);
 		~ModuleLoader();
 		bool	loadModule(const std::string &name);
 		bool	unloadModule(const std::string &name);
+
 	private:
-		//std::vector<AModule *>	_modules;
+		std::map<std::string, std::pair<ModuleDL *, apouche::AModule *>> _modules;
+		const std::string	_basedir;
+		apouche::Logger		_logger;
+		
 	};
+
 }
